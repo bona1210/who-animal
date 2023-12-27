23744e9f86afd818b3401d6f68532ec129f7a400
bona <jimmy249872041@gmail.com>
2023-12-27 18:06:40 +0800

    8test

--- index.js
+++ index.js
@@ -28,6 +28,30 @@ app.get("/ts/:studentId", async (req, res) => {
         res.status(500).json({ error: "Internal server error" });
     }
 });
+//------------------------------------------------------------------
+
+app.get("/ts/:studentId", async (req, res) => {
+    // 從動態路由參數中取得 studentId
+    const studentId = req.params.studentId;
+
+    try {
+        const document = await db.collection("location").doc(studentId).get();
+
+        if (!document.exists) {
+            res.status(404).json({ error: "Document not found", studentId });
+            return;
+        }
+
+        const data = { id: document.id, ...document.data() };
+        res.json(data);
+    } catch (error) {
+        console.error("Error retrieving document:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+
+
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
 
