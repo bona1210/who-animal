5ca146a089c02597666a9f7f4834b938260df73a
bona <jimmy249872041@gmail.com>
2023-12-27 18:14:09 +0800

    9test

--- index.js
+++ index.js
@@ -15,16 +15,7 @@ app.get("/ts/:studentId", async (req, res) => {
 
     try {
         const document = await db.collection("ClassA").doc(studentId).get();
-        const document1 = await db.collection("location").doc(studentId).get();
 
-        if (!document1.exists) {
-            res.status(404).json({ error: "Document not found", studentId });
-            return;
-        }
-
-        const data1 = { id: document1.id, ...document1.data() };
-        res.json(data1);
-    
         if (!document.exists) {
             res.status(404).json({ error: "Document not found", studentId });
             return;
@@ -39,6 +30,25 @@ app.get("/ts/:studentId", async (req, res) => {
 });
 //------------------------------------------------------------------
 
+app.get("/ts/:studentId", async (req, res) => {
+    // 從動態路由參數中取得 studentId
+    const studentId = req.params.studentId;
+
+    try {
+        const document = await db.collection("location").doc(studentId).get();
+
+        if (!document.exists) {
+            res.status(404).json({ error: "Document not found", studentId });
+            return;
+        }
+
+        const data = { id: document.id, ...document.data() };
+        res.json(data);
+    } catch (error) {
+        console.error("Error retrieving document:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
 
 
 
